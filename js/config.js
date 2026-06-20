a794b3150dfe44515777d443238329dc6cc49438
Nyquist <nyquist64@yahoo.com>
2026-06-20 21:00:07 +0200

    config changed

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
 	var appConfig = {
 		'base' : 'http://' + location.hostname + ":" + location.port,
 	}	
-} else if(location.hostname === "cvetkovic-70.github.io") {
+} else if(location.hostname === "proniveau.github.io") {
 	var appConfig = {
-		'base': 'https://cvetkovic-70.github.io/slavica-cvetkovic',
+		'base': 'proniveau.github.io/landingpage',
 	}
 } else {
 	var appConfig = {
